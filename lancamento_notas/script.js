27388b27fdcc2a551d8d2b48c5b8bdcee91f7f57
charlesantunes <charleswcantunes@gmail.com>
2023-04-28 21:31:00 -0300

    adicionando primeira  mudanças no js da página lancamentos_notas

--- lancamento_notas/script.js
+++ lancamento_notas/script.js
@@ -1,1 +1,26 @@
-console.log('Lançamento de Notas ok')
+const ck1 = document.querySelector("#ck1")
+const ck2 = document.querySelector("#ck2")
+const ck3 = document.querySelector("#ck3")
+const ck4 = document.querySelector("#ck4")
+const ck5 = document.querySelector("#ck5")
+
+ck1.addEventListener('click',pintarPatas )
+
+function pintarPatas() {
+   var input = document.getElementsByTagName('input');				
+			
+		if(ck1.checked) {
+			ck2.nextSibling.nodeValue = input.value  + " [checked]"
+            console.log(ck2)
+		}
+		else {
+			item.nextSibling.nodeValue = item.id;
+		}
+	}
+
+  
+    
+    
+    // let at = document.createAttribute("class")
+    // at.value = "pataClicada";
+    // ck2.setAttributeNode(at)
