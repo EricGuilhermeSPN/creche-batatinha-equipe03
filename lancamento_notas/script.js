3a958fae4b093071fb093f787b65973a38f8ba89
charlesantunes <charleswcantunes@gmail.com>
2023-04-29 00:59:31 -0300

    primeira modificação do script preencher patas

--- lancamento_notas/script.js
+++ lancamento_notas/script.js
@@ -1,23 +1,82 @@
-const ck1 = document.querySelector("#ck1")
-const ck2 = document.querySelector("#ck2")
-const ck3 = document.querySelector("#ck3")
-const ck4 = document.querySelector("#ck4")
-const ck5 = document.querySelector("#ck5")
+const selectSocializacao = document.querySelectorAll('[data-socializacao]')
+// let todasPatas = selectSocializacao.forEach(item => item.id)
 
-ck1.addEventListener('click',pintarPatas )
+//addEventListener('click',pintarPatas)
+for (let i = 0; i < selectSocializacao.length; i++) {
+    const pataDavez = selectSocializacao[i]
+    const pata = pataDavez.classList[1]; // classe ck
+    //console.log(pata)
+    
+    const pataId = `#c-${pata}`;
+    //console.log(pataId);
 
-function pintarPatas() {
-   var input = document.getElementsByTagName('input');				
+    pataDavez.onclick = function () {
+        pintarPatas(pataId)
+    } 
+}
+
+function pintarPatas(pataId) {
+    console.log(pataId)
+    // selectSocializacao.forEach(item => console.log(item))	
 			
-		if(ck1.checked) {
-			ck2.nextSibling.nodeValue = input.value  + " [checked]"
-            console.log(ck2)
-		}
-		else {
-			item.nextSibling.nodeValue = item.id;
-		}
-	}
-
+		if(pataId == '#c-ck5') {
+			//check()
+            console.log('clicado')
+            document.getElementById("c-ck4").checked = true;
+            document.getElementById("c-ck1").checked = true;
+            document.getElementById("c-ck2").checked = true;
+            document.getElementById("c-ck3").checked = true;
+		    }else{
+			//uncheck()
+			console.log('não clicado')
+            document.getElementById("c-ck1").checked = false;
+            document.getElementById("c-ck2").checked = false;
+            document.getElementById("c-ck3").checked = false;
+            document.getElementById("c-ck4").checked = false;
+		    }
+       /* if(pataId == '#c-ck4') {
+			//check()
+            console.log('clicado')
+            document.getElementById("c-ck1").checked = true;
+            document.getElementById("c-ck2").checked = true;
+            document.getElementById("c-ck3").checked = true;
+		    }else{
+			//uncheck()
+			console.log('não clicado')
+            document.getElementById("c-ck1").checked = false;
+            document.getElementById("c-ck2").checked = false;
+            document.getElementById("c-ck3").checked = false;
+		    }
+        if(pataId == '#c-ck3') {
+			//check()
+            console.log('clicado')
+            document.getElementById("c-ck1").checked = true;
+            document.getElementById("c-ck2").checked = true;
+		    }else{
+			//uncheck()
+			console.log('não clicado')
+            document.getElementById("c-ck1").checked = false;
+            document.getElementById("c-ck2").checked = false;
+		    }
+        if(pataId == '#c-ck2') {
+			//check()
+            console.log('clicado')
+            document.getElementById("c-ck1").checked = true;
+		    }else{
+			//uncheck()
+			console.log('não clicado')
+            document.getElementById("c-ck1").checked = false;
+		    }
+            */
+}
+/*
+function check() {
+    document.getElementById("ck1").checked = true;
+}
+function uncheck() {
+    document.getElementById("ck1").checked = false;
+}
+*/
   
     
     
