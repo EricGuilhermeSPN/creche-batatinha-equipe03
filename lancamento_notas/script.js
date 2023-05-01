a6e781c98fa9ee37391c8515d216eaadf16a4aa5
charlesantunes <charleswcantunes@gmail.com>
2023-05-01 01:24:42 -0300

    patas seleção com js lancamento_notas finalizada

--- lancamento_notas/script.js
+++ lancamento_notas/script.js
@@ -1,85 +1,79 @@
-const selectSocializacao = document.querySelectorAll('[data-socializacao]')
-// let todasPatas = selectSocializacao.forEach(item => item.id)
-
-//addEventListener('click',pintarPatas)
-for (let i = 0; i < selectSocializacao.length; i++) {
-    const pataDavez = selectSocializacao[i]
-    const pata = pataDavez.classList[1]; // classe ck
-    //console.log(pata)
-    
-    const pataId = `#c-${pata}`;
-    //console.log(pataId);
-
-    pataDavez.onclick = function () {
-        pintarPatas(pataId)
-    } 
-}
-
-function pintarPatas(pataId) {
-    console.log(pataId)
-    // selectSocializacao.forEach(item => console.log(item))	
-			
-		if(pataId == '#c-ck5') {
-			//check()
-            console.log('clicado')
-            document.getElementById("c-ck4").checked = true;
-            document.getElementById("c-ck1").checked = true;
-            document.getElementById("c-ck2").checked = true;
-            document.getElementById("c-ck3").checked = true;
-		    }else{
-			//uncheck()
-			console.log('não clicado')
-            document.getElementById("c-ck1").checked = false;
-            document.getElementById("c-ck2").checked = false;
-            document.getElementById("c-ck3").checked = false;
-            document.getElementById("c-ck4").checked = false;
-		    }
-       /* if(pataId == '#c-ck4') {
-			//check()
-            console.log('clicado')
-            document.getElementById("c-ck1").checked = true;
-            document.getElementById("c-ck2").checked = true;
-            document.getElementById("c-ck3").checked = true;
-		    }else{
-			//uncheck()
-			console.log('não clicado')
-            document.getElementById("c-ck1").checked = false;
-            document.getElementById("c-ck2").checked = false;
-            document.getElementById("c-ck3").checked = false;
-		    }
-        if(pataId == '#c-ck3') {
-			//check()
-            console.log('clicado')
-            document.getElementById("c-ck1").checked = true;
-            document.getElementById("c-ck2").checked = true;
-		    }else{
-			//uncheck()
-			console.log('não clicado')
-            document.getElementById("c-ck1").checked = false;
-            document.getElementById("c-ck2").checked = false;
-		    }
-        if(pataId == '#c-ck2') {
-			//check()
-            console.log('clicado')
-            document.getElementById("c-ck1").checked = true;
-		    }else{
-			//uncheck()
-			console.log('não clicado')
-            document.getElementById("c-ck1").checked = false;
-		    }
-            */
-}
 /*
-function check() {
-    document.getElementById("ck1").checked = true;
-}
-function uncheck() {
-    document.getElementById("ck1").checked = false;
-}
+let patas = document.querySelectorAll(".star-icon")
+//let patas2 = document.querySelectorAll(".star-icon")
+
+document.addEventListener('click', function (e) {
+    let patasSelect = e.target.classList //peguei todas classes de onde foi clicado
+
+    let classDoClick = e.target.classList[1] //peguei a segunda classe
+    let num = e.target.getAttribute('data-avaliacao') // imprime o valor do data-avaliacao
+    let nomeId = `${classDoClick}${num}` //juntei o nome da segunda classe da tag mais o nº da data-avaliaao = id 
+
+    console.log(patasSelect)
+    console.log(classDoClick)
+    console.log(nomeId)
+
+   
+
+    if (nomeId == 'soc1') {
+        document.getElementById(`${nomeId}`).classList.add("ativo")
+        console.log('estou no s5')
+    }
+    if (nomeId == 'soc2') {
+        document.getElementById(`${nomeId}`).classList.add("ativo")
+        console.log('estou no s5')
+    }
+    if (nomeId == 'soc3') {
+        document.getElementById(`${nomeId}`).classList.add("ativo")
+        console.log('estou no s5')
+    }
+    if (nomeId == 'soc4') {
+        document.getElementById(`${nomeId}`).classList.add("ativo")
+        console.log('estou no s5')
+    }
+    if (nomeId == 'soc5') {
+        document.getElementById(`${nomeId}`).classList.add("ativo")
+        console.log('estou no s5')
+    }
+   
+
+    // if (patasSelect.contains("ativo")) {
+    //     patas.forEach((e)=>e.classList.remove('ativo'))
+    //     console.log('pintado')        
+    //     console.log(classDoClick)
+    // }
+    //patasSelect.classList.add('ativo')
+    // if (!patasSelect.contains("ativo") && patasSelect.contains("son")) {
+    //     patas2.forEach((e)=>e.classList.remove('ativo'))
+    //     console.log('pintado')        
+    //     console.log(classDoClick)
+    //     return patas2
+    // }
+
+    
+    if(e.target.value == null){ //se o clica for fora das classes
+        patas.forEach((e)=>e.classList.remove('ativo'))
+        patas.forEach((e)=>e.classList.add('ativo'))
+        console.log('nada')
+    }
+    console.log(e.target.getAttribute('data-avaliacao')) // avaliação p/ banco de dados
+})
+
 */
-  
-    
-    
-    // let at = document.createAttribute("class")
-    // at.value = "pataClicada";
-    // ck2.setAttributeNode(at)
+
+//
+document.addEventListener('click', function (e) {
+    const nomeClasse = e.target.classList[0] //peguei a primeira classe da lista pelo click do mouse
+
+    const patas = document.querySelectorAll(`.patas${nomeClasse} li`)
+    console.log(patas)
+
+    patas.forEach((element,indice1)=>{
+        element.addEventListener('click',()=>{
+            patas.forEach((element,indice2)=>{
+                indice1 >= indice2 ? element.classList.add("active") : element.classList.remove("active")
+            })
+        })
+    })
+
+})
